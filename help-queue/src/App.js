8904ecece9edfd9a997d2c73d0871fe10304175e
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-04 15:19:03 -0700

    Change App.js

--- help-queue/src/App.js
+++ help-queue/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <h1>Help Queue</h1>
+        <h3>3a</h3>
+        <h3>Thato and Haley</h3>
+        <p><em>Firebase entries not saving!</em></p>
+        <hr/>
+    </>
 }
 
 export default App;
