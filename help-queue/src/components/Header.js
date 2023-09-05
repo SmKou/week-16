7feb6fc81d1d3e207c737800cc07f2825b792222
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-04 22:53:14 -0700

    Add sample image

--- help-queue/src/components/Header.js
+++ help-queue/src/components/Header.js
@@ -1,5 +1,10 @@
+import ticketImg from '../img/tickets.png';
+
 function Header() {
-    return <h1>Help Queue</h1>
+    return <>
+        <h1>Help Queue</h1>
+        <img src={ticketImg} alt="An image of tickets" />
+    </>
 }
 
 export default Header;
