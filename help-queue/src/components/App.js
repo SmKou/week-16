932fbad26fb7147de3ac5676b0178fcd2491baa5
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-04 20:33:24 -0700

    Add app and header components

--- help-queue/src/components/App.js
+++ help-queue/src/components/App.js
@@ -1,8 +1,12 @@
+import Header from './Header';
+
 function App() {
+    const name = "Thato";
+    const name2 = "Haley";
     return <>
-        <h1>Help Queue</h1>
+        <Header />
         <h3>3a</h3>
-        <h3>Thato and Haley</h3>
+        <h3>{name} and {name2}</h3>
         <p><em>Firebase entries not saving!</em></p>
         <hr/>
     </>
---
+++ help-queue/src/components/Header.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1>Help Queue</h1>
+}
+
+export default Header;
