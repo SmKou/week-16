ed648a12234e89aa50a76c41015b8a17aa19e811
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-17 02:34:32 -0700

    Remove experiments (read on vue development practices)

--- help-queue-vue/src/TicketApp/AddTicketForm.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import ReusableForm from './ReusableForm.vue';
-</script>
-
-<template>
-    <ReusableForm />
-</template>
-
-<style scoped></style>
--- help-queue-vue/src/main.ts
+++ help-queue-vue/src/main.ts
@@ -1,28 +1,5 @@
 import { createApp } from 'vue';
 import './style.css'
 import App from './App.vue';
-import HeaderApp from './HeaderApp/App.vue'
-import TicketApp from './TicketApp/App.vue'
-import AddTicketForm from './TicketApp/AddTicketForm.vue'
-import EditTicketForm from './TicketApp/EditTicketForm.vue'
-import TicketDetail from './TicketApp/TicketDetail.vue'
-import TicketList from './TicketApp/TicketList.vue'
-
-const Header = createApp(HeaderApp)
-Header.mount('#header')
-
-const routes = [
-    { path: '/', component: TicketList },
-    { path: '/create', component: AddTicketForm },
-    { path: '/details', component: TicketDetail },
-    { path: '/edit', component: EditTicketForm }
-]
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHashHistory(),
-    routes
-})
-const Ticket = Vue.createApp(TicketApp)
-Ticket.use(router);
-Ticket.mount('#ticket-control')
 
 createApp(App).mount('#app')
