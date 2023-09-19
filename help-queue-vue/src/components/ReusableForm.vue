ba3c0e28f049657c840fb0b37ba4738db001bf38
SmKou <66178856+SmKou@users.noreply.github.com>
2023-09-19 16:01:14 -0700

    WIP Adapt Help Queue to Vue (ts)

---
+++ help-queue-vue/src/components/ReusableForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+    submitHandler: (e: any) => void,
+    buttonText: string,
+    ticket?: TicketIxF
+}
+const props = defineProps<Props>()
+function studentNames() {
+    return props.ticket ? props.ticket.names.join(' and ') : ''
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="submitHandler">
+        <label for="names">Student Names:</label>
+        <input type="text" id="names" name="names" placeholder="Who is having the issue?" :value="studentNames" />
+        <label for="location">Location:</label>
+        <input type="text" id="location" name="location" placeholder="Where are you?" :value="props.ticket.location" />
+        <label for="issue">Issue:</label>
+        <textarea id="issue" name="issue" placeholder="What is your issue?">{{props.ticket.issue}}</textarea>
+        <input type="submit" :value="props.buttonText" />
+    </form>
+</template>
---
+++ help-queue-vue/src/components/TicketController.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from "vue";
+interface TicketIxF {
+    location: string,
+    names: string[],
+    issue: string,
+    id: string
+}
+const pages = { Add: 'add', Edit: 'edit', Detail: 'detail', List: 'list' }
+const page = ref(pages.List)
+const ticketList = ref<TicketIxF[]>([])
+const
+</script>
+
+<template>
+    <main>
+        <section v-if="page === pages.Add"></section>
+        <section v-else-if="page === pages.Detail"></section>
+        <section v-else-if="page === pages.Edit"></section>
+        <section v-else></section>
+    </main>
+</template>
+
+<style></style>
